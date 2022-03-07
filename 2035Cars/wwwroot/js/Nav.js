bade904b7fde1c548b27cf3e87449ed47b7f5c88
KrzysztofBorejko <krzysiek.13.krzysiek@gmail.com>
2022-03-07 16:24:40 +0100

    Working on Navbar and Footer - part 3

--- 2035Cars/wwwroot/js/Nav.js
+++ 2035Cars/wwwroot/js/Nav.js
@@ -1,1 +1,15 @@
 ﻿
+// OPEN HAMBURGER MENU
+document.getElementById('open-mobile-nav-links').addEventListener('click', () => {
+    var mobileNavLinksBackground = document.getElementById('mobile-nav-links-background');
+
+    mobileNavLinksBackground.classList.toggle('mobile-nav-links-bg-active');
+    
+});
+
+// CLOSE HAMBURGER MENU
+document.getElementById('close-mobile-nav-links').addEventListener('click', () => {
+    var mobileNavLinksBackground = document.getElementById('mobile-nav-links-background');
+
+    mobileNavLinksBackground.classList.remove('mobile-nav-links-bg-active');
+});
